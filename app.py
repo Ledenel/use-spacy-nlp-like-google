1140fbfee0660245a1a46b7b79e3945a6df0eb57
Ledenel <ledenelintelli@gmail.com>
2021-08-10 12:31:12 +0000

    add word mode

--- app.py
+++ app.py
@@ -255,12 +255,14 @@ def nlp():
         query="type:doc",
         text="假如你是李华，你打算给Tom写一封信，信中描述你在中国的生活。要求不少于200词。",
         text_only="no",
+        word_mode="no",
         subtree_limit="500",    
     ), **request.args, **request.form
     }
     params.setdefault("limit", len(params["text"]))
     params["subtree_limit"] = int(params["subtree_limit"])
     params["text_only"] = params["text_only"] == "yes"
+    params["word_mode"] = params["word_mode"] == "yes"
     print("request done", params["query"], perf_counter() - _st); _st = perf_counter()
     doc = model_dict[params["model"]](params["text"])
     print("nlp parse done, to write", perf_counter() - _st); _st = perf_counter()
@@ -309,7 +311,11 @@ def nlp():
         print("search done", perf_counter() - _st); _st = perf_counter()
         search_items_result = [search_items[hit["id"]] for hit in results]
         if params["text_only"]:
-            search_items_result = [item["text"].replace(" ", "") for item in search_items_result]
+            search_items_result = [item["text"] for item in search_items_result]
+            if params["word_mode"]:
+                search_items_result = [t.replace(" ", "") for t in search_items_result]
+            else:
+                search_items_result = [t.split(" ") for t in search_items_result]
         return jsonify(search_items_result)
 
 
